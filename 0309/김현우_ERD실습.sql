4f1ff6626a44f107516f10bc417fb7fe30eec791
hwk44 <peos0n@kakao.com>
2023-03-09 17:55:32 +0900

    0309 정규화.

---
+++ 0309/김현우_ERD실습.sql
@@ -0,0 +1,33 @@
+insert 학생 values('160001','이지금','01');
+insert 학생 values('160002','박평호','02');
+insert 학생 values('160003','조세운','03');
+insert 학생 values('160004','김명오','01');
+insert 학생 values('160005','조나단','02');
+insert 학생 values('160006','하이리','03');
+insert 학생 values('160007','이우영','03');
+insert 학생 values('160008','김나은','01');
+insert 학생 values('160009','혁거세','01');
+insert 학생 values('160010','김유신','02');
+
+insert 전공 values('01','화학공학과');
+insert 전공 values('02','컴퓨터공학과');
+insert 전공 values('03','국어국문학과');
+
+insert 수강내역 values('160001', 'K20005');
+insert 수강내역 values('160001', 'K20012');
+insert 수강내역 values('160001', 'K20030');
+insert 수강내역 values('160001', 'Y00132');
+
+insert 수강내역 values('160002', 'K20004');
+insert 수강내역 values('160002', 'K20012');
+insert 수강내역 values('160002', 'Y00133');
+
+insert 수강내역 values('160003', 'K20005');
+insert 수강내역 values('160003', 'Y00132');
+
+insert 수강과목 values('K20004','기초수학과학1','3');
+insert 수강과목 values('K20005','생활속윤리','3');
+insert 수강과목 values('K20012','세계사속인문학','3');
+insert 수강과목 values('K20030','인문학적독서법','2');
+insert 수강과목 values('Y00132','기초생활체육','3');
+insert 수강과목 values('Y00133','취미로배우는바둑','2');
